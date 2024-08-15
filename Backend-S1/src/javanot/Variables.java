894eae107e82d888a9f46348f68d7241a3331096
SantiagoQv <totyquinterovalencia@gmail.com>
2024-08-15 07:25:37 -0500

    commit clase 1 j
    repaso de variables

---
+++ Backend-S1/src/javanot/Variables.java
@@ -0,0 +1,15 @@
+package javanot;
+
+public class Variables {
+    public static void main(String[] args) {
+        byte moveLeft; // declarando la variable
+
+        moveLeft = 127;
+
+        short moveLeft2 = (short) moveLeft;
+
+        System.out.println(moveLeft2);
+
+        moveLeft2 = 130; 
+    }
+}
